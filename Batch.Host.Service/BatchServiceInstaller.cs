8d7958cff4bb8f6dc96dfda1a6f3be45c1f91a55
RomanTymoshenko <46889623+RomanTymoshenko@users.noreply.github.com>
2021-01-19 18:58:15 +0200

    Add project files.

---
+++ Batch.Host.Service/BatchServiceInstaller.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ServiceProcess;
+using System.Configuration.Install;
+
+namespace Batch.Host.Service
+{
+    [RunInstaller(true)]
+    public partial class BatchServiceInstaller : System.Configuration.Install.Installer
+    {
+        ServiceInstaller _serviceInstaller;
+        ServiceProcessInstaller _processInstaller;
+        public BatchServiceInstaller()
+        {
+            InitializeComponent();
+            _serviceInstaller = new ServiceInstaller();
+            _processInstaller = new ServiceProcessInstaller();
+            
+            _processInstaller.Account = ServiceAccount.LocalSystem;
+            _serviceInstaller.StartType = ServiceStartMode.Manual;
+            _serviceInstaller.ServiceName = "BatchService";
+            Installers.Add(_processInstaller);
+            Installers.Add(_serviceInstaller);
+        }
+    }
+}
---
+++ Batch.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Batch.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("Batch.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d17c0784-79e5-44ac-90ef-5fbc8f2c5ee0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
